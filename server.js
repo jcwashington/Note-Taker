7218c4b46420fc07928d62b657b9da7c435c5983
JCWashington <jasmine.washington412@gmail.com>
2022-01-21 22:00:33 -0500

    adding server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.argv.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// custom middleware for api and html routes
+const apiRoutes = require('./routes/apiRoutes');
+app.use(apiRoutes);
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use(htmlRoutes);
+
+// server listener
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
