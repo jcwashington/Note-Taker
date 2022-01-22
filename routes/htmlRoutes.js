7ed4195e11a2a46fea2bfe1a92403304ed39d1ba
JCWashington <jasmine.washington412@gmail.com>
2022-01-21 23:11:07 -0500

    rearranged routes so that /notes route would function

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// route for homepage
-app.get('/*', (req, res) => 
-    res.sendFile(path.join(__dirname, '../public/index.html')));
 
 // route for /notes
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/notes.html')));
+// route for homepage
+app.get('/*', (req, res) => 
+    res.sendFile(path.join(__dirname, '../public/index.html')));
+
+
 
 module.exports = app;
